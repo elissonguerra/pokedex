525a8e165025f5be52ca909c1467b1b046169577
elissonguerra <elisson.guerra@gmail.com>
2024-02-20 20:37:30 -0300

    Criado o código de leitura de arquivo json e desserialização dos pokémons.

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.Models;
 
@@ -15,7 +16,13 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            List<Pokemon> pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+            return View(pokemons);
+        }
+        return View(pokemons);
     }
 
     public IActionResult Privacy()
