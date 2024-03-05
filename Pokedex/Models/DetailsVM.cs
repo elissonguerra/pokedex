a430f2a205983ac0571e35181ffd352064bc66db
elissonguerra <elisson.guerra@gmail.com>
2024-03-05 20:33:19 -0300

    Parei na página 153

---
+++ Pokedex/Models/DetailsVM.cs
@@ -0,0 +1,11 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Pokedex.Models;
+
+public class DetailsVM
+{
+    public Pokemon Anterior {get; set;}
+    public Pokemon Atual {get; set;}
+    public Pokemon Proximo {get; set;}
+    public List<Tipo> Tipos {get; set;}
+}
